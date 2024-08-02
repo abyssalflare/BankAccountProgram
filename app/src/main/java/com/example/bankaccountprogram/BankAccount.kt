e809c8950d42c6e6fdc955b7477f539c559f9883
Jeryl Goh <jerylgoh2003@gmail.com>
2024-08-02 15:32:29 +0800

    Added Classes and Files
    defined BankMain file and BankAccount class

---
+++ app/src/main/java/com/example/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,21 @@
+package com.example.bankaccountprogram
+
+class BankAccount(var accountHolder: String, var balance: Double) {
+
+    private val transactionHistory = mutableListOf<String>();
+
+    fun deposit(amount: Double)
+    {
+
+    }
+
+    fun withdraw(amount: Double)
+    {
+
+    }
+
+    fun displayTransactionHistory()
+    {
+
+    }
+}
